0d6eda233c8392d253b9a9ba8896d13c079f3864
Riyaz <mohriyaz17@gmail.com>
2023-03-22 23:04:04 -0700

    adding javaScript files

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+const editPlayer1BtnElement = document.querySelector("#edit-playerOne-btn");
+const editPlayer2BtnElement = document.querySelector("#edit-playerTwo-btn");
+
+
+
+editPlayer1BtnElement.addEventListener("click",openPlayerConfig);
+editPlayer2BtnElement.addEventListener("click",openPlayerConfig);
