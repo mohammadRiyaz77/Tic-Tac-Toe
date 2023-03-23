e5610357b487f3c611e054cfcda392e67249fe4c
Riyaz <mohriyaz17@gmail.com>
2023-03-22 23:17:35 -0700

    showing & hiding configuration overlay

--- scripts/config.js
+++ scripts/config.js
@@ -1,1 +1,10 @@
-function openPlayerConfig() {}
+const openPlayerConfig = ()=> {
+    playerConfigOverlayElement.style.display = "block";
+    backDropElement.style.display = "block";
+
+
+}
+const closePlayerConfig = ()=>{
+    playerConfigOverlayElement.style.display = "none";
+    backDropElement.style.display = "none";
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,13 @@
+const playerConfigOverlayElement = document.querySelector("#config-overlay");
+const backDropElement = document.querySelector("#backdrop");
+
 const editPlayer1BtnElement = document.querySelector("#edit-playerOne-btn");
 const editPlayer2BtnElement = document.querySelector("#edit-playerTwo-btn");
 
-
+const cancelCongigBtnElement = document.querySelector("#cancel-config-btn");
 
 editPlayer1BtnElement.addEventListener("click",openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click",openPlayerConfig);
+
+cancelCongigBtnElement.addEventListener("click", closePlayerConfig);
+backDropElement.addEventListener("click" , closePlayerConfig);
